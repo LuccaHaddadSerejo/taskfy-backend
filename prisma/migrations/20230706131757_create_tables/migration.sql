3000be1d8ceb9b0bd6200ac435d2568003f475d1
LuccaHaddadSerejo <hslucca@gmail.com>
2023-07-06 10:19:34 -0300

    feat: generated migrations and created database folder with prisma service inside

---
+++ prisma/migrations/20230706131757_create_tables/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "tasks" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "done" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "tasks_pkey" PRIMARY KEY ("id")
+);
